32a3bbc6f3d3ba0b53c4ec44cac3bcdad0e52f04
krist-jin <krist333@126.com>
2014-12-07 01:27:10 -0500

    Delete random_graph_gen.py

--- random_graph_gen.py
+++
@@ -1,6 +0,0 @@
-import networkx as nx
-g = nx.fast_gnp_random_graph(n=1300, p=0.03)
-output = open("output.txt", "w")
-for e in g.edges():
-    output.write(str(e[0])+" "+str(e[1])+"\n")
-output.close()
