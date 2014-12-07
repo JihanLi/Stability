4885fb08898d319e3d1d6ac70ed9bbee56b24adc
JihanLi <jh9228.li@gmail.com>
2014-12-07 15:28:48 -0500

    ini

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Random;
 
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
@@ -12,9 +14,12 @@ import org.jgrapht.graph.SimpleWeightedGraph;
 public class Graph {
 	
 	private SimpleWeightedGraph<String, DefaultWeightedEdge>  graph = new SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class); 
+	private HashSet<String> VertexSet1 = new HashSet<String>();
+	private HashSet<String> VertexSet2 = new HashSet<String>();
+	
 	
 	//Read the data into the graph.
-	public void ReadFile(String filename) throws IOException
+	public void ReadGraph(String filename, boolean training) throws IOException
 	{
 		File file = new File(filename);
     	InputStreamReader reader = null;
@@ -26,37 +31,264 @@ public class Graph {
         BufferedReader br = new BufferedReader(reader);
         String line = "";
         
-        try {
-			while((line = br.readLine()) != null)
-			{
-				String[] nodes= line.split(" ");
-				if(nodes.length < 3)
-					continue;
-				if(!graph.containsVertex(nodes[0]))
-       		 	{
-       			 	graph.addVertex(nodes[0]);
-       		 	}
-       		 	if(!graph.containsVertex(nodes[1]))
-       		 	{
-       		 		graph.addVertex(nodes[1]);
-       		 	}
-       		 	if(!graph.containsEdge(nodes[0], nodes[1]) && !(nodes[0].equals(nodes[1])))
-       		 	{
-       		 		graph.addEdge(nodes[0], nodes[1]);
-       		 		graph.setEdgeWeight(graph.getEdge(nodes[0], nodes[1]), Double.parseDouble(nodes[2]));
-       		 	}				
+        if(training == true)
+        {
+	        try {
+					while((line = br.readLine()) != null)
+					{
+						String[] nodes= line.split(" ");
+						if(nodes.length < 2)
+							continue;
+						if(!graph.containsVertex(nodes[0]))
+		       		 	{
+		       			 	graph.addVertex(nodes[0]);
+		       		 	}
+		       		 	if(!graph.containsVertex(nodes[1]))
+		       		 	{
+		       		 		graph.addVertex(nodes[1]);
+		       		 	}
+		       		 	if(!graph.containsEdge(nodes[0], nodes[1]) && !(nodes[0].equals(nodes[1])))
+		       		 	{
+		       		 		graph.addEdge(nodes[0], nodes[1]);
+		       		 	}				
+					}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}        	
+        }
+        else
+        {
+	        try {
+					while((line = br.readLine()) != null)
+					{
+						String[] nodes= line.split(" ");
+						if(nodes.length < 3)
+							continue;
+						if(!graph.containsVertex(nodes[0]))
+		       		 	{
+		       			 	graph.addVertex(nodes[0]);
+		       		 	}
+		       		 	if(!graph.containsVertex(nodes[1]))
+		       		 	{
+		       		 		graph.addVertex(nodes[1]);
+		       		 	}
+		       		 	if(!graph.containsEdge(nodes[0], nodes[1]) && !(nodes[0].equals(nodes[1])))
+		       		 	{
+		       		 		graph.addEdge(nodes[0], nodes[1]);
+		       		 		graph.setEdgeWeight(graph.getEdge(nodes[0], nodes[1]), Double.parseDouble(nodes[2]));
+		       		 	}				
+					}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        }
         reader.close();
         br.close();
 	}	
-
 	
-	
-	public void update()
+	public SimpleWeightedGraph<String, DefaultWeightedEdge> getGraph()
 	{
+		return graph;
+	}
+	
+	public void setGraph(SimpleWeightedGraph<String, DefaultWeightedEdge> g)
+	{
+		this.graph = g;
+	}
+	
+	public void generateGraph(double ratio)
+	{
+		Random rand = new Random();
+		if(ratio == 1)
+		{
+			for(DefaultWeightedEdge e : graph.edgeSet())
+			{
+				double weight = rand.nextGaussian()/6+0.5;
+				while(weight <= 0 || weight > 1)
+				{
+					weight = rand.nextGaussian()/6+0.5;
+				}
+				graph.setEdgeWeight(e, weight);
+			}
+		}
+		else
+		{
+			int numVertex = (int) (graph.vertexSet().size()*(1+ratio)/2);
+			int i = 0;
+			while(i < numVertex)
+			{
+				Integer	ram = (int)(Math.random()*graph.vertexSet().size());
+				String ras = ram.toString();
+				if(!VertexSet1.contains(ras))
+				{
+					VertexSet1.add(ras);
+					i++;
+				}
+			}
+			for(String v : graph.vertexSet())
+			{
+				if(!VertexSet1.contains(v))
+				{
+					VertexSet2.add(v);
+				}
+			}
+			
+		}
+		
+		for(DefaultWeightedEdge e : graph.edgeSet())
+		{
+			String v1 = graph.getEdgeSource(e);
+			String v2 = graph.getEdgeTarget(e);
+
+			if(VertexSet1.contains(v1) && VertexSet1.contains(v2))
+			{
+				double weight;
+				double pos = 0;
+				double neg = 0;
+				for(DefaultWeightedEdge ev1 : graph.edgesOf(v1))
+				{
+					String source = graph.getEdgeSource(ev1);
+					if(source.equals(v1))
+					{
+						source = graph.getEdgeTarget(ev1);
+					}
+					if(VertexSet1.contains(source))
+					{
+						pos++;
+					}
+					else
+						neg++;
+				}
+				for(DefaultWeightedEdge ev2 : graph.edgesOf(v2))
+				{
+					String source = graph.getEdgeSource(ev2);
+					if(source.equals(v1))
+					{
+						source = graph.getEdgeTarget(ev2);
+					}
+					if(VertexSet1.contains(source))
+					{
+						pos++;
+					}
+					else
+						neg++;
+				}
+				
+				weight = pos/(pos+neg);
+				graph.setEdgeWeight(e, weight);
+			}
+			else if(VertexSet2.contains(v1) && VertexSet2.contains(v2))
+			{
+				double weight;
+				double pos = 0;
+				double neg = 0;
+				for(DefaultWeightedEdge ev1 : graph.edgesOf(v1))
+				{
+					String source = graph.getEdgeSource(ev1);
+					if(source.equals(v1))
+					{
+						source = graph.getEdgeTarget(ev1);
+					}
+					if(VertexSet2.contains(source))
+					{
+						pos++;
+					}
+					else
+						neg++;
+				}
+				for(DefaultWeightedEdge ev2 : graph.edgesOf(v2))
+				{
+					String source = graph.getEdgeSource(ev2);
+					if(source.equals(v2))
+					{
+						source = graph.getEdgeTarget(ev2);
+					}
+					if(VertexSet2.contains(source))
+					{
+						pos++;
+					}
+					else
+						neg++;
+				}
+				
+				weight = pos/(pos+neg);
+				graph.setEdgeWeight(e, weight);
+			}
+			else if(VertexSet1.contains(v1) && VertexSet2.contains(v2))
+			{
+				double weight;
+				double pos = 0;
+				double neg = 0;
+				for(DefaultWeightedEdge ev1 : graph.edgesOf(v1))
+				{
+					String source = graph.getEdgeSource(ev1);
+					if(source.equals(v1))
+					{
+						source = graph.getEdgeTarget(ev1);
+					}
+					if(VertexSet1.contains(source))
+					{
+						pos++;
+					}
+					else
+						neg++;
+				}
+				for(DefaultWeightedEdge ev2 : graph.edgesOf(v2))
+				{
+					String source = graph.getEdgeSource(ev2);
+					if(source.equals(v2))
+					{
+						source = graph.getEdgeTarget(ev2);
+					}
+					if(VertexSet2.contains(source))
+					{
+						pos++;
+					}
+					else
+						neg++;
+				}
+				
+				weight = -neg/(pos+neg);
+				graph.setEdgeWeight(e, weight);
+			}
+			else if(VertexSet2.contains(v1) && VertexSet1.contains(v2))
+			{
+				double weight;
+				double pos = 0;
+				double neg = 0;
+				for(DefaultWeightedEdge ev1 : graph.edgesOf(v1))
+				{
+					String source = graph.getEdgeSource(ev1);
+					if(source.equals(v1))
+					{
+						source = graph.getEdgeTarget(ev1);
+					}
+					if(VertexSet2.contains(source))
+					{
+						pos++;
+					}
+					else
+						neg++;
+				}
+				for(DefaultWeightedEdge ev2 : graph.edgesOf(v2))
+				{
+					String source = graph.getEdgeSource(ev2);
+					if(source.equals(v2))
+					{
+						source = graph.getEdgeTarget(ev2);
+					}
+					if(VertexSet1.contains(source))
+					{
+						pos++;
+					}
+					else
+						neg++;
+				}
+				
+				weight = -neg/(pos+neg);
+				graph.setEdgeWeight(e, weight);
+			}
+		}
 		
 	}
 	
--- Graph/Main.java
+++ Graph/Main.java
@@ -1,16 +1,39 @@
 package Graph;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
 
 public class Main {
 
 	//The main function running the algorithm.
 	public static void main(String[] args) throws IOException
 	{
-		String filename = "graph.txt";
-		Graph g = new Graph();
+		String filename = "TrG.txt";
 		
-		g.ReadFile(filename);
+		for(int rate = 1; rate <= 10;rate++)
+		{
+			Graph g = new Graph();
+			String wFilename = "TwG_"+rate+"0.txt";
+			File f = new File(wFilename);
+			PrintWriter output = new PrintWriter(new FileWriter(f,false));
+			
+			g.ReadGraph(filename,true);
+			g.generateGraph(rate/10.0);
+			SimpleWeightedGraph<String, DefaultWeightedEdge>  graph = g.getGraph();
+			for(DefaultWeightedEdge e : graph.edgeSet())
+			{
+				output.println(graph.getEdgeSource(e)+" "+graph.getEdgeTarget(e)+" "+graph.getEdgeWeight(e));
+				output.flush();
+			}
+			
+			output.close();
+		}
+				
 	}
 		
 }
