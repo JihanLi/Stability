b82f58b34958e8a4c2ae5eb6dd46e377e4a4761a
krist-jin <krist333@126.com>
2014-12-13 01:28:41 -0500

    clique part1

--- Graph/genCompleteGraph.py
+++ Graph/genCompleteGraph.py
@@ -1,14 +1,14 @@
 import networkx as nx
 
 def genCompleteGraph(n):
-    f = open("complete_graph_"+str(n)+".txt", "w")
+    f = open("clique_data\complete_graph_"+str(n)+".txt", "w")
     g = nx.complete_graph(n)
     for i in g.edges():
         f.write(str(i[0])+" "+str(i[1])+"\n")
     f.close()
 
 def main():
-    for n in xrange(10, 110, 10):
+    for n in xrange(100, 10000, 100):
         print n
         genCompleteGraph(n)
 
