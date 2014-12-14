f47f5f07a9171ccefedf4d538376e03b287548cf
JihanLi <jh9228.li@gmail.com>
2014-12-14 12:51:13 -0500

    最终版main

--- Graph/Main.java
+++ Graph/Main.java
@@ -12,18 +12,14 @@ import org.jgrapht.graph.SimpleWeightedGraph;
 public class Main {
 
 	//The main function running the algorithm.	
-	//平衡度以及新团大小改变
 	public static void main(String[] args) throws Exception
 	{
-		
 		String path0 = "source";
-		double newEdgeNumber = 0.02;
-		
 		File dir0 = new File(path0);
 		if (!dir0.exists()) 
 			dir0.mkdirs();
 		
-		String path1 = "test";
+		String path1 = "result";
 		File dir1 = new File(path1);
 		if (!dir1.exists()) 
 			dir1.mkdirs();
@@ -33,7 +29,12 @@ public class Main {
 		if (!dir2.exists()) 
 			dir2.mkdirs();
 		
-		DecimalFormat df=new DecimalFormat("#.#");
+		int[] size1 = {1,5,10,20,30,40,50,60};
+		double[] size2 = {0,0.05,0.1,0.15,0.2};
+		
+		DecimalFormat df1=new DecimalFormat("#.#");
+		DecimalFormat df2=new DecimalFormat("#.###");
+		
 		String filename = path0+File.separator+"TrG.txt";
 		String tr1 = path2+File.separator+"triangle_data.txt";
 		String tr2 = path2+File.separator+"edge2tri_data.txt";
@@ -45,12 +46,12 @@ public class Main {
 		g.exportTriangles(tr1,tr2);
 		System.out.println(g.all_triangles.size());
 
-		
+		//改变原图的平衡度
 		double balanceRatio = 0;
 		while(balanceRatio <= 1)
 		{	
 			
-			String br = df.format(balanceRatio);
+			String br = df1.format(balanceRatio);
 			g = new Graph();
 			g.ReadGraph(filename, true);
 			g.importTriangles(tr1, tr2);
@@ -60,15 +61,17 @@ public class Main {
 			g.WriteGraph(tr3);
 			System.out.println(g.getBalanceRatio());	
 			
-			
-			for(int i = 1; i <= 10;i++)
+			//固定新加团与原图形成新边的数量，改变新加团的大小
+			for(int i = 0; i < 8;i++)
 			{
+				double newEdgeNumber = 0.02;
+				String en = df2.format(newEdgeNumber);
+				
 				g = new Graph();
 				g.ReadGraph(tr3, false);
 				
-				String clicName = path0+File.separator+"clique"+Integer.toString(i)+".txt";
-				String diffFile = path1+File.separator+"diff_BR"+br+"_EN"
-							+df.format(newEdgeNumber)+"_SZ"+Integer.toString(i)+".txt";
+				String clicName = path0+File.separator+"clique"+Integer.toString(size1[i])+".txt";
+				String diffFile = path1+File.separator+"diff_BR"+br+"_SZ"+Integer.toString(size1[i])+"_EN"+en+".txt";
 				
 				Graph c = new Graph();
 				c.ReadGraph(clicName, true);
@@ -84,74 +87,38 @@ public class Main {
 				
 				System.out.println(iter);
 			}
+			
+			//固定新加团的大小，改变新加团与原图形成新边的数量
+			for(int i = 0; i < 5;i++)
+			{		
+				double newEdgeNumber = size2[i];
+				String en = df2.format(newEdgeNumber);
+			
+				g = new Graph();
+				g.ReadGraph(tr3, false);
+	
+				String clicName = path0+File.separator+"clique2.txt";
+				String diffFile = path1+File.separator+"diff_BR"+br+"_EN"+en+"_SZ2.txt";
+				
+				Graph c = new Graph();
+				c.ReadGraph(clicName, true);
+				c.generateClique(0.5);  //the number of positive edges in the clique
+	
+				
+				g.addClique(c, newEdgeNumber);    //Change the number of newly added edges between clique and the graph.
+				g.findTriangles();
+				
+				System.out.println(g.all_triangles.size());
+				System.out.println(g.getBalanceRatio());
+				
+				int iter = g.stabilize(diffFile);
+				
+				System.out.println(iter);		
+			
+			}
 						
 			
 			balanceRatio += 0.1;	
-		}
-	}
-	
-	//新团与网络连边数量改变
-	public static void main2(String[] args) throws Exception
-	{
-		
-		String path0 = "source";
-		double balanceRatio = 0.7;
-		
-		File dir0 = new File(path0);
-		if (!dir0.exists()) 
-			dir0.mkdirs();
-		
-		String path1 = "result";
-		File dir1 = new File(path1);
-		if (!dir1.exists()) 
-			dir1.mkdirs();
-		
-		String path2 = "temp";
-		File dir2 = new File(path2);
-		if (!dir2.exists()) 
-			dir2.mkdirs();
-		
-		DecimalFormat df1=new DecimalFormat("#.###");
-		DecimalFormat df2=new DecimalFormat("#.#");
-		String filename = path0+File.separator+"TrG.txt";
-			
-		Graph g = new Graph();
-/*		g.ReadGraph(filename, true);
-		g.findTriangles();*/
-
-		String tr3 = path2+File.separator+"tempGraph_EN.txt";
-/*		g.generateWeight(balanceRatio);  //Change the balance ratio.
-		g.WriteGraph(tr3);
-		System.out.println(g.getBalanceRatio());	*/
-
-		double newEdgeNumber = 0;
-		while(newEdgeNumber < 0.2)
-		{				
-			String en = df1.format(newEdgeNumber);
-		
-			g = new Graph();
-			g.ReadGraph(tr3, false);
-
-			String clicName = path0+File.separator+"clique2.txt";
-			String diffFile = path1+File.separator+"diff_EN"+en+"_BR"
-						+df2.format(balanceRatio)+"_SZ2.txt";
-			
-			Graph c = new Graph();
-			c.ReadGraph(clicName, true);
-			c.generateClique(0.5);  //the number of positive edges in the clique
-
-			
-			g.addClique(c, newEdgeNumber);    //Change the number of newly added edges between clique and the graph.
-			g.findTriangles();
-			
-			System.out.println(g.all_triangles.size());
-			System.out.println(g.getBalanceRatio());
-			
-			int iter = g.stabilize(diffFile);
-			
-			System.out.println(iter);		
-		
-			newEdgeNumber += 0.05;	
 		}
 	}
 		
